e4d798f9331cc2080f7775b38588a0bd3b584c73
Demi Hayashi <demi.h@me.com>
2022-09-02 16:26:39 -0700

    set up initial front-end architecture & template engine

--- server.js
+++ server.js
@@ -1,15 +1,24 @@
+const path = require('path');
 const express = require('express');
 const sequelize = require('./config/connection');
 const routes = require('./controllers');
+// set up handlebars.js
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({});
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
 
 // turn on routes
 app.use(routes);
+
+// set up handlebars.js
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
 // turn on connection to db and server
 sequelize.sync({ force: false }).then(() => {
