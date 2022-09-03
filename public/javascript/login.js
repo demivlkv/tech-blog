bc665b18b390dcca14149b3222b532492abaa2cc
Demi Hayashi <demi.h@me.com>
2022-09-02 19:58:15 -0700

    add session within back end

---
+++ public/javascript/login.js
@@ -0,0 +1,54 @@
+async function signupForm(event) {
+    event.preventDefault();
+
+    const username = document.getElementById('username-signup').value.trim();
+    const email = document.getElementById('email-signup').value.trim();
+    const password = document.getElementById('password-signup').value.trim();
+
+    if (username && email && password) {
+        const response = await fetch('/api/users', {
+            method: 'post',
+            body: JSON.stringify({
+                username,
+                email,
+                password
+            }),
+            headers: { 'Content-Type': 'application/json' }
+        });
+
+        // check response status
+        if (response.ok) {
+            console.log('Success!');
+        } else {
+            alert(response.statusText);
+        }
+    }
+};
+
+async function loginForm(event) {
+    event.preventDefault();
+
+    const username = document.getElementById('username-login').value.trim();
+    const password = document.getElementById('password-login').value.trim();
+
+    if (username && password) {
+        const response = await fetch('/api/users/login', {
+            method: 'post',
+            body: JSON.stringify({
+                username,
+                password
+            }),
+            headers: { 'Content-Type': 'application/json' }
+        });
+
+        // check response status
+        if (response.ok) {
+            document.location.replace('/');
+        } else {
+            alert(response.statusText);
+        }
+    }
+};
+
+document.querySelector('.signup-form').addEventListener('submit', signupForm);
+document.querySelector('.login-form').addEventListener('submit', loginForm);
--- server.js
+++ server.js
@@ -5,6 +5,19 @@ const routes = require('./controllers');
 // set up handlebars.js
 const exphbs = require('express-handlebars');
 const hbs = exphbs.create({});
+const session = require('express-session');
+
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const sess = {
+    secret: 'The secret formula for the Krabby Patty',
+    cookie: {},
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+        db: sequelize
+    })
+};
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -12,6 +25,7 @@ const PORT = process.env.PORT || 3001;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(session(sess));
 
 // turn on routes
 app.use(routes);
