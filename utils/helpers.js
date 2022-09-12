8ceb38cd9cff4fe896b224a8b43fc470ac2f0318
Demi Hayashi <demi.h@me.com>
2022-09-11 17:12:17 -0700

    test timezone bug

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,6 +1,6 @@
 module.exports = {
     format_date: date => {
-        return `${new Date(date).toLocaleDateString('en-US')} @ ${new Date(date).toLocaleTimeString('en-US', { hour: '2-digit', minute: '2-digit', timeZone: 'PST', timeZoneName: 'short' })}`;
+        return `${new Date(date).toLocaleDateString('en-US', { timeZone: 'PST'})} @ ${new Date(date).toLocaleTimeString('en-US', { hour: '2-digit', minute: '2-digit', timeZone: 'PST', timeZoneName: 'short' })}`;
     },
     format_plural: (word, amount) => {
         if (amount !== 1) {
